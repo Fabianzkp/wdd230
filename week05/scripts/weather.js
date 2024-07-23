26df032bd3420b9f7568fd9e04d384cda25bb63f
Fabianzkp <fabianzkp@gmail.com>
2024-07-23 20:58:45 +0100

    weather-api

---
+++ week05/scripts/weather.js
@@ -0,0 +1,37 @@
+// Select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// API key and coordinates for Trier, Germany
+const apiKey = '19986686d43ac7cac75b81de240e71b0';
+const lat = 49.749992;
+const lon = 6.637143;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); 
+      displayResults(data); 
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+// Call the apiFetch function to execute the API request
+apiFetch();
